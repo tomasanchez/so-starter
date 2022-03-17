833c521430d9b030e54d56fa4dcda19a90c0e9d6
R. Nicolás Savinelli <rsavinelli@frba.utn.edu.ar>
2022-03-17 13:19:34 -0300

    feat(server): add server beta core functionalities

---
+++ server/include/core/core.functionality.h
@@ -0,0 +1,28 @@
+/**
+ * core.functionality.h
+ *
+ * @file Core functionality for the server module
+ * @author Tomás A. Sánchez and R. Nicolás Savinelli
+ * @since  03.17.2022
+ */
+
+#pragma once
+
+#include "lib.h"
+#include "server.h"
+
+#define RUNNING true
+
+typedef struct MODULE
+{
+    servidor_t server;
+    bool status;
+} t_module;
+
+int init(void);
+
+int start(void);
+
+int run(void *(*procedure)(void *));
+
+int stop(void);
---
+++ server/src/main.c
@@ -0,0 +1,25 @@
+/**
+ * main.c
+ *
+ * @file  Módulo
+ * @author Tomás A. Sánchez and R. Nicolás Savinelli
+ * @since  03.17.2022
+ */
+
+#include "core.functionality.h"
+#include "module.procedures.h"
+
+int main(void)
+{
+    if (init() == SUCCESS)
+    {
+        if (start() == SUCCESS)
+        {
+            run(procedures);
+        }
+    }
+
+    stop();
+
+    return EXIT_SUCCESS;
+}
