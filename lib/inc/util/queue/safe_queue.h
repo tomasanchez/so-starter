92b8eaa191e1987400861e5c8c09fc2f99760502
lschirripa <lschirripa@frba.utn.edu.ar>
2022-03-26 15:06:59 -0300

    add safe include push & pop

--- lib/inc/util/queue/safe_queue.h
+++ lib/inc/util/queue/safe_queue.h
@@ -1,4 +1,55 @@
 #pragma once
 
 #include <pthread.h>
-#include <commons/collections/list.h>
+#include <commons/collections/queue.h>
+
+// 
+
+// 	/**
+// 	* @NAME: queue_push
+// 	* @DESC: Agrega un elemento al final de la cola
+// 	*/
+// 	void queue_push(t_queue *, void *element);
+
+// 	/**
+// 	* @NAME: queue_pop
+// 	* @DESC: quita el primer elemento de la cola
+// 	*/
+// 	void *queue_pop(t_queue *);
+
+// 
+typedef struct SafeQueue
+{
+
+	// ! Private:
+
+	/**
+	 * @brief Thread safety ensurer.
+	 * @private
+	 */
+	pthread_mutex_t _mtx;
+
+	/**
+	 * @brief queue implementation.
+	 * @private
+	 */
+	t_queue *_queue;
+
+	// ! Public:
+
+} safe_queue_t;
+
+
+/**
+ * @brief safely add an element at the last of the queue.
+ *
+ * @return .
+ */
+safe_queue_t safe_queue_push(t_queue *, void *element);
+
+/**
+ * @brief safely remove the last elementf of the queue.
+ *
+ * @return .
+ */
+safe_queue_t *safe_queue_pop(t_queue *);
